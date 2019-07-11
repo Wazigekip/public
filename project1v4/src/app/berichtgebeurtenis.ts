17f06707008465c8f312f19f297ad237f98a3bc5
Wazigekip <49661305+Wazigekip@users.noreply.github.com>
2019-07-11 22:11:29 +0200

    Update berichtgebeurtenis.ts

--- project1v4/src/app/berichtgebeurtenis.ts
+++ project1v4/src/app/berichtgebeurtenis.ts
@@ -5,4 +5,12 @@ export class Berichtgebeurtenis {
 	speler: string;
 	titel: string;
 	gebeurtenis: string;
+  
+  constructor(spel,dag,speler,titel,gebeurtenis){
+		this.spel=spel;
+		this.dag=dag;
+		this.speler=speler;
+		this.titel=titel;
+		this.gebeurtenis=gebeurtenis;
+	}
 }
